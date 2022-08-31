3bc260d444baadbb78bc544759d379c3f6fe514d
Sebastian Sulbaran <sebastian.sulbaran@hotmail.com>
2022-08-30 22:57:51 -0300

    Lesson 5. Separation of responsabilities, we made a controller to set values and the view file containing all html code

--- index.php
+++ index.php
@@ -1,29 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        header{
-            text-align: center;
-            padding: 2em;
-            background-color: #e3e3e3;
-        }
-    </style>
-</head>
-<body>
-    <header>
-        <h1>
-            <?php
-                //sanitizing input
-                echo "Hello you ! " . (htmlspecialchars($_GET['name']) ? htmlspecialchars($_GET['name']) : "user") . ". But if you trying to put code it wont work.";
-            ?>
-        </h1>
-        <!-- 
-            <?= "Hello, " . htmlspecialchars($_GET['name']); ?>
-         -->
-    </header>
-</body>
-</html>
+<?php
+
+//separacion de responsabilidades, esto es un controlador y se llama a una "vista"
+// en el controlador, se establecen las propiedades que necesita la view
+
+$greetings = "Hello you ! " . (htmlspecialchars($_GET['name']) ? htmlspecialchars($_GET['name']) : "user");
+
+
+require("index.view.php");
