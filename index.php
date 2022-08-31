3254bf3e2cf63dc78eeffdd0e9cf8fe6b0f35262
Sebastian Sulbaran <sebastian.sulbaran@hotmail.com>
2022-08-30 22:47:50 -0300

    Lesson 4. Using php in html, superglobals and sanitizing

--- index.php
+++ index.php
@@ -1,19 +1,29 @@
-<?php
-
-/*one way to print string*/
-echo 'Hello World';
-echo "<br>";
-$greeting = "Hi!";
-$name = "Sebastian Sulbaran";
-echo "<br>";
-
-/* Second way to print a string*/
-echo "$greeting, $name";
-echo "<br>";
-
-/* Third way to print a string */
-echo "Hi!, " . $name;
-echo "<br>";
-
-/*Fourth way optional using brackets*/
-echo "{$greeting}, {$name}";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        header{
+            text-align: center;
+            padding: 2em;
+            background-color: #e3e3e3;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>
+            <?php
+                //sanitizing input
+                echo "Hello you ! " . (htmlspecialchars($_GET['name']) ? htmlspecialchars($_GET['name']) : "user") . ". But if you trying to put code it wont work.";
+            ?>
+        </h1>
+        <!-- 
+            <?= "Hello, " . htmlspecialchars($_GET['name']); ?>
+         -->
+    </header>
+</body>
+</html>
