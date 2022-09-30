1413818119ff8b14ec39129f796f12af10f622f3
Sebastian Sulbaran <sebastian.sulbaran@hotmail.com>
2022-09-30 00:58:39 -0300

    Lesson 17. Optimizing views with partials

--- views/index.view.php
+++ views/index.view.php
@@ -1,26 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        header{
-            text-align: center;
-            padding: 2em;
-            background-color: #e3e3e3;
-        }
-    </style>
-</head>
-<body>
-    <header>
-        <h1>
-            <?php
-                echo $greetings;
-            ?>
-        </h1>
-    </header>
+
+<?php require("partials/head.php") ?>
 
     <h2>List of task from Database</h2>
     <ul>
@@ -41,5 +20,5 @@
 
     </ul>
 
-</body>
-</html>
+<?php require("partials/footer.php") ?>
+
---
+++ views/partials/nav-bar.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact Us</a></li>
+        <li><a href="/about-culture">About Culture</a></li>
+        <li><a href="/">Home</a></li>
+        
+    </ul>
+</nav>
